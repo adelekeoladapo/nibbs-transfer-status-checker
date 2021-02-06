a6020b1be7e4d9eae72987ca74cea8d51a442c90
Dapo Adeleke <adelekeoladapo@gmail.com>
2021-02-06 21:36:16 +0100

    Externalized configuration

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"ajocard/status-indicator/controller"
 	"github.com/labstack/echo/v4"
+	"github.com/joho/godotenv"
+	"log"
 	"net/http"
 )
 
 func main() {
 	e := echo.New()
+
+	/* Load configuration */
+	err:= godotenv.Load()
+	if err != nil {
+		log.Panic("Could not load app configuration")
+	}
+
+	/* App Router */
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello AjoCard")
 	})
